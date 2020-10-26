920cae40f86b4d3bb1b351e9c674659a28ceb400
anujjoshi <anuj@groktor.net>
2020-10-26 15:48:38 +0100

    Initial Commit

---
+++ DropDownAnimation/DropDownAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  DropDownAnimationApp.swift
+//  DropDownAnimation
+//
+//  Created by Anuj Joshi on 26/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct DropDownAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
